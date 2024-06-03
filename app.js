c76080b721348f8614040dd3c1439124a295888d
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2024-06-03 13:05:14 +0300

    Play Game Function

--- app.js
+++ app.js
@@ -1,53 +1,72 @@
-let humanScore=0,
-    computerScore=0;
+const para = document.querySelector(".test");
+const btn = document.querySelector("button");
+const compScore = document.querySelector(".computerScore");
+const playerScore = document.querySelector(".playerScore");
+const header = document.querySelector("h1");
+let counter = 0;
 
-function getComputerChoice(){
-    let result = Math.round(Math.random()*3);
-    if(result===0){
+function getComputerChoice() {
+    let result = Math.round(Math.random() * 3);
+    if (result === 0) {
         return "rock";
-    } else if(result===1){
+    } else if (result === 1) {
         return "paper";
     } else {
         return "scissors";
     }
 }
 
-function getHumanChoice(){
-    const choices=["rock","paper","scissors"];
-    let userChoice=prompt("rock paper or scissors?","");
-    var userChoice1=userChoice.toLowerCase();
-    if(choices.includes(userChoice1))
-        {
-            if(userChoice1=="rock")
-                {
-                    return "rock";
-                } else if(userChoice1=="paper")
-                    {
-                        return "paper";
-                    } else if(userChoice1=="scissors")
-                    {
-                        return "scissors";
-                    }
-        } else{
-            alert("Please enter either rock, paper or scissors");
+function getHumanChoice() {
+    let userChoice = prompt("Rock Paper or Scissors?", "");
+
+    if (userChoice.toLowerCase() === "rock") {
+        return "rock";
+    } else if (userChoice.toLowerCase() == "paper") {
+        return "paper";
+    } else if (userChoice.toLowerCase() == "scissors") {
+        return "scissors";
+    }
+    else {
+        const error=document.createElement("p");
+        const message=document.createTextNode("Please Enter either Rock, Paper or Scissors");
+        try{
+            throw new error
         }
-    
+    }
+
 }
 
-function playRound(humanChoice,computerChoice)
-{
-    if((humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="scissors" && computerChoice==="paper")||(humanChoice==="paper" && computerChoice==="rock"))
-        {
-            alert(`You Win! ${humanChoice.charAt(0).toUpperCase()} beats ${computerChoice.charAt(0).toUpperCase()}`);
-        } else if((humanChoice==="rock" && computerChoice==="paper") || (humanChoice==="paper" && computerChoice==="scissors")||(humanChoice==="scissors" && computerChoice==="rock"))
-            {
-                alert(`You Lose! ${humanChoice.charAt(0).toUpperCase()} beats ${computerChoice.charAt(0).toUpperCase()}`);
-            } else{
-                alert("It's a draw");
-            }
+function playGame() {
+
+    let humanScore = 0,
+        computerScore = 0;
+    function playRound() {
+        counter++;
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        if ((humanSelection === "rock" && computerSelection === "scissors") || (humanSelection === "scissors" && computerSelection === "paper") || (humanSelection === "paper" && computerSelection === "rock")) {
+            humanScore++;
+            para.textContent = `You win! ${humanSelection.charAt(0).toUpperCase()+humanSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)}`;
+        } else if ((humanSelection === "rock" && computerSelection === "paper") || (humanSelection === "paper" && computerSelection === "scissors") || (humanSelection === "scissors" && computerSelection === "rock")) {
+            computerScore++;
+            para.textContent = `You Lose! ${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)} beats ${humanSelection.charAt(0).toUpperCase()+humanSelection.slice(1)}`;
+        } else {
+            para.textContent = `It is a draw.`;
+        }
+    }
+    playRound();
+    if (counter === 5) {
+        playerScore.textContent = `Player Score: ${humanScore}`;
+        compScore.textContent = `Computer Score: ${computerScore}`;
+        if (humanScore > computerScore) {
+            header.textContent = "You Win! Congratulations";
+        } else if (humanScore < computerScore) {
+            header.textContent = "You've Lost! Better Luck Next Time";
+        } else {
+            header.textContent = "It ended at a Draw.";
+        }
+    }
+
 }
 
-const humanSelection=getHumanChoice();
-const computerSelection=getComputerChoice();
-
-playRound(humanSelection,computerSelection);
+btn.addEventListener("click", playGame);
