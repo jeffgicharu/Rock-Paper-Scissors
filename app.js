224bb412c4681fa1c41011e20d6c2e84ec80650a
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2024-06-10 08:35:02 +0300

    Logic to play single round

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ function getHumanChoice(){
 
 let humanScore=0;
 let computerScore=0;
+
+function playRound(humanChoice,computerChoice){
+    humanChoice=humanChoice.toLowerCase();
+
+    if(humanChoice===computerChoice){
+        console.log("It's a tie");
+        return "tie";
+    } else if(
+        (humanChoice==="rock" && computerChoice==="scissors") ||
+        (humanChoice==="paper" && computerChoice==="rock") ||
+        (humanChoice==="scissors" && computerChoice==="paper")
+    ){
+        humanScore++;
+        console.log(`You win! ${humanChoice.charAt(0).toUpperCase()+humanChoice.slice(1)} beats ${computerChoice}.`);
+        return "human";
+    } else{
+        computerScore++;
+        console.log(`You lose! ${computerChoice.charAt(0).toUpperCase()+computerChoice.slice(1)} beats ${humanChoice}.`);
+        return "computer";
+    }
+}
