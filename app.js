f6031e20d7383e3cff5c3266a0d048978521901c
jeffgicharu <jeff.kaharu@students.jkuat.ac.ke>
2024-06-10 08:23:27 +0300

    Get human choice function

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ function getComputerChoice(){
     const randomIndex=Math.floor(Math.random()*choices.length);
     return choices[randomIndex];
 }
+
+function getHumanChoice(){
+    let choice=prompt("Please enter your choice (rock, paper or scissors):");
+    return choice.toLowerCase();
+}
+
